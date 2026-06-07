dac76e13ba3edb62850cdf5ce6654373749c1f94
Su33SeaFlanker <wasabigaming121@gmail.com>
2026-06-07 10:53:32 +0200

    Update asciifetch.py

--- src/asciifetch/asciifetch.py
+++ src/asciifetch/asciifetch.py
@@ -49,7 +49,7 @@ def main():
         usersSystem = platform.system()
         if(usersSystem == "Linux"):
             editor = os.environ.get('EDITOR', 'nano')
-            subprocess.run([editor, config_file])
+            subprocess.run([editor, str(config_file)])
             return
         if(usersSystem == "Windows"):
             os.startfile(config_file)
